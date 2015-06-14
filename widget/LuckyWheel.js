7732030cf51a82dc732bc74bd88a742f7064d133
Gary Lo <logap@me.com>
2015-06-14 21:38:39 +0800

    config of wheel can be override

--- widget/LuckyWheel.js
+++ widget/LuckyWheel.js
@@ -8,7 +8,7 @@
 		// super class
 		this.Container_constructor();
 
-		var CONFIG = {
+		var config = {
 			size:400, // size in px
 			framerate:60,
 			numOfSector: 16, // number of
@@ -28,12 +28,13 @@
 		var deltaDegree = 0;
 
 		this.init = function () {
+			stage.clear();
 			// Draw sectors
 			var angle = 0;
 			while (angle < 360) {
 				self.addSector({
 					angle:angle,
-					config:CONFIG
+					config:config
 				}, function (sector) {
 					// add to parent
 					self.addChild(sector);
@@ -44,10 +45,10 @@
 			}
 			// reposition the rotating center and the position
 			self.set({
-				regX : CONFIG.size,
-				regY : CONFIG.size,
-				x : CONFIG.size / 2,
-				y : CONFIG.size / 2
+				regX : config.size,
+				regY : config.size,
+				x : config.size / 2,
+				y : config.size / 2
 			});
 
 			// Draw a stop line
@@ -71,10 +72,10 @@
 			var angle = opts.angle;
 
 			var sectorContainer = new createjs.Container(),
-				sectorAngle = 360 / CONFIG.numOfSector,
+				sectorAngle = 360 / config.numOfSector,
 				startAngle = 0,
 				endAngle = Math.min(360, sectorAngle) * Math.PI/180,
-				shape = new createjs.Shape().set({x:CONFIG.size, y:CONFIG.size}),
+				shape = new createjs.Shape().set({x:config.size, y:config.size}),
 				id = Math.floor(angle / sectorAngle),
 				color = Math.random() * 0xFFFFFF,
 				textColor = 0x000000,
@@ -85,15 +86,15 @@
 			shape.graphics.f(createjs.Graphics.getRGB(color));
 			shape.graphics.beginStroke(createjs.Graphics.getRGB(color - 0x000000)).setStrokeStyle(1);
 			shape.graphics.moveTo(0,0);
-			shape.graphics.arc(0,0,CONFIG.size/2,startAngle,endAngle);
+			shape.graphics.arc(0,0,config.size/2,startAngle,endAngle);
 
 
 			// rotate to vertical
 			shape.rotation = -1 * (sectorAngle/2) - 90 ;
 
 			// hard tuning on relative position
-			text.x = CONFIG.size * 2.97 / 3;
-			text.y = CONFIG.size * 1.7 / 3;
+			text.x = config.size * 2.97 / 3;
+			text.y = config.size * 1.7 / 3;
 			text.regX = Math.min(50,sectorAngle)/5;
 
 			//add the children to the sector container
@@ -103,10 +104,10 @@
 			// config the container center & rotation
 			sectorContainer.set({
 				id : id,
-				x : CONFIG.size,
-				y : CONFIG.size,
-				regX : CONFIG.size,
-				regY : CONFIG.size,
+				x : config.size,
+				y : config.size,
+				regX : config.size,
+				regY : config.size,
 				rotation: angle,
 				startAngle : angle,
 				endAngle : angle + sectorAngle
@@ -120,10 +121,10 @@
 
 		this.event = {
 			acceleration : function () {
-				var ANGULAR = CONFIG.physics.angular,
+				var ANGULAR = config.physics.angular,
 					MAX_SPEED = ANGULAR.maxSpeed,
-					ACC = ANGULAR.acceleration / CONFIG.framerate,
-					INITAL_ACC = ANGULAR.initialAcceleration / CONFIG.framerate;
+					ACC = ANGULAR.acceleration / config.framerate,
+					INITAL_ACC = ANGULAR.initialAcceleration / config.framerate;
 
 				deltaDegree = (deltaDegree + ACC) > MAX_SPEED ?
 					MAX_SPEED :
@@ -137,11 +138,11 @@
 				self.rotation = (self.rotation + deltaDegree) % 360;
 			},
 			deceleration : function () {
-				var angular = CONFIG.physics.angular,
+				var angular = config.physics.angular,
 					MAX_SPEED = angular.maxSpeed,
-					DEC = CONFIG.slowEndEffect && deltaDegree < MAX_SPEED / 4 ?
-					angular.deceleration/ CONFIG.framerate /4 :
-					angular.deceleration / CONFIG.framerate;
+					DEC = config.slowEndEffect && deltaDegree < MAX_SPEED / 4 ?
+					angular.deceleration/ config.framerate /4 :
+					angular.deceleration / config.framerate;
 
 				deltaDegree = (deltaDegree - DEC) > 0 ? deltaDegree - DEC : 0;
 				self.rotation = (self.rotation + deltaDegree) % 360;
@@ -170,7 +171,11 @@
 
 
 		this.getConfig = function () {
-			return CONFIG;
+			return config;
+		};
+
+		this.setConfig = function (cfg) {
+			config = _.extend(config,cfg);
 		};
 
 		return this;
